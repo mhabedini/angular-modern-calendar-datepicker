66094b186a29b5730142fb262d911683cdb04eac
Mohammad Hosein Abedini <m.h.abedini@gmail.com>
2024-05-19 12:32:15 +0330

    Implement month picker

--- projects/angular-persian-datepicker/src/lib/service/hijri-date-service.ts
+++ projects/angular-persian-datepicker/src/lib/service/hijri-date-service.ts
@@ -6,126 +6,131 @@ import {getWeekFirstAndLastDays, processDateRange} from "../helper/date-helper";
 
 export class HijriDateService implements DateServiceInterface {
 
-    translate = {
-        goToToday: 'برو به امروز',
-        nextMonth: 'ماه بعد',
-        previousMonth: 'ماه قبل',
-    };
+  translate = {
+    goToToday: 'برو به امروز',
+    nextMonth: 'ماه بعد',
+    previousMonth: 'ماه قبل',
+  };
 
-    config = {
-        rtl: true,
-        WeekendDays: [6]
-    }
+  config = {
+    rtl: true,
+    WeekendDays: [6]
+  }
 
-    constructor() {
-        momentHijri.locale('ar-SA', {useGregorianParser: true})
-    }
+  constructor() {
+    momentHijri.locale('ar-SA', {useGregorianParser: true})
+  }
 
-    months(): string[] {
-        return [
-            "محرم",
-            "صفر",
-            "ربیع الاول",
-            "ربیع الثانی",
-            "جمادی الاول",
-            "جمادی الثانی",
-            "رجب",
-            "شعبان",
-            "رمضان",
-            "شوال",
-            "ذیقعده",
-            "ذیحجه",
-        ];
-    }
+  months(): string[] {
+    return [
+      "محرم",
+      "صفر",
+      "ربیع الاول",
+      "ربیع الثانی",
+      "جمادی الاول",
+      "جمادی الثانی",
+      "رجب",
+      "شعبان",
+      "رمضان",
+      "شوال",
+      "ذیقعده",
+      "ذیحجه",
+    ];
+  }
 
-    monthsShort(): string[] {
-        return [
-            "محرم",
-            "صفر",
-            "ربیع الاول",
-            "ربیع الثانی",
-            "جمادی الاول",
-            "جمادی الثانی",
-            "رجب",
-            "شعبان",
-            "رمضان",
-            "شوال",
-            "ذیقعده",
-            "ذیحجه",
-        ];
-    }
+  monthsShort(): string[] {
+    return [
+      "محرم",
+      "صفر",
+      "ربیع الاول",
+      "ربیع الثانی",
+      "جمادی الاول",
+      "جمادی الثانی",
+      "رجب",
+      "شعبان",
+      "رمضان",
+      "شوال",
+      "ذیقعده",
+      "ذیحجه",
+    ];
+  }
 
-    weekdays(): string[] {
-        return [
-            "السبت",
-            "الأحد",
-            "الأثنين",
-            "الثلاثاء",
-            "الأربعاء",
-            "الخميس",
-            "الجمعه",
-        ];
-    }
+  weekdays(): string[] {
+    return [
+      "السبت",
+      "الأحد",
+      "الأثنين",
+      "الثلاثاء",
+      "الأربعاء",
+      "الخميس",
+      "الجمعه",
+    ];
+  }
 
-    weekdaysShort(): string[] {
-        return [
-            "السبت",
-            "الأحد",
-            "الأثنين",
-            "الثلاثاء",
-            "الأربعاء",
-            "الخميس",
-            "الجمعه",
-        ];
-    }
+  weekdaysShort(): string[] {
+    return [
+      "السبت",
+      "الأحد",
+      "الأثنين",
+      "الثلاثاء",
+      "الأربعاء",
+      "الخميس",
+      "الجمعه",
+    ];
+  }
 
-    daysInMonth(date: string): any[] {
-        const hijriDate = momentHijri(date)
-        const iDaysInMonth = hijriDate.iDaysInMonth();
-        const iMonth = hijriDate.iMonth()
-        const iYear = hijriDate.iYear()
+  daysInMonth(date: string): any[] {
+    const hijriDate = momentHijri(date)
+    const iDaysInMonth = hijriDate.iDaysInMonth();
+    const iMonth = hijriDate.iMonth()
+    const iYear = hijriDate.iYear()
 
-        const startDate = moment(momentHijri(`${iYear}/${iMonth + 1}/01`, 'iYYYY/iMM/iDD').format('YYYY/MM/DD'))
-        const endDate = moment(momentHijri(`${iYear}/${iMonth + 1}/${iDaysInMonth}`, 'iYYYY/iMM/iDD').format('YYYY/MM/DD'))
-        const year = moment(startDate).year()
+    const startDate = moment(momentHijri(`${iYear}/${iMonth + 1}/01`, 'iYYYY/iMM/iDD').format('YYYY/MM/DD'))
+    const endDate = moment(momentHijri(`${iYear}/${iMonth + 1}/${iDaysInMonth}`, 'iYYYY/iMM/iDD').format('YYYY/MM/DD'))
+    const year = moment(startDate).year()
 
-        console.log(startDate);
-        console.log(endDate);
+    console.log(startDate);
+    console.log(endDate);
 
-        const [weeks, startDateDayBefore, endDateDayAfter] = processDateRange(startDate, endDate)
+    const [weeks, startDateDayBefore, endDateDayAfter] = processDateRange(startDate, endDate)
 
-        const momentRange: any = extendMoment(moment)
-        const calendar: any[] = []
-        const now = moment()
+    const momentRange: any = extendMoment(moment)
+    const calendar: any[] = []
+    const now = moment()
 
-        weeks.forEach((week: any) => {
-            let [firstWeekDay, lastWeekDay] = getWeekFirstAndLastDays(weeks, week, year)
+    weeks.forEach((week: any) => {
+      let [firstWeekDay, lastWeekDay] = getWeekFirstAndLastDays(weeks, week, year)
 
-            const weekRange = momentRange.range(firstWeekDay, lastWeekDay)
-            const finalWeeks: any[] = []
-            Array.from(weekRange.by('day')).forEach((day: any, index) => {
-                const iDate = momentHijri(day.format('YYYY/MM/DD'), 'YYYY/MM/DD')
-                console.log(iDate.format('YYYY/MM/DD'))
-                finalWeeks.push({
-                    gDate: day,
-                    date: iDate,
-                    weekIndex: index,
-                    day: iDate.iDate(),
-                    isToday: day.isSame(now, "day"),
-                    isForCurrentMonth: day.isBetween(startDateDayBefore, endDateDayAfter)
-                })
-            })
-            calendar.push(finalWeeks);
+      const weekRange = momentRange.range(firstWeekDay, lastWeekDay)
+      const finalWeeks: any[] = []
+      Array.from(weekRange.by('day')).forEach((day: any, index) => {
+        const iDate = momentHijri(day.format('YYYY/MM/DD'), 'YYYY/MM/DD')
+        console.log(iDate.format('YYYY/MM/DD'))
+        finalWeeks.push({
+          gDate: day,
+          date: iDate,
+          weekIndex: index,
+          day: iDate.iDate(),
+          isToday: day.isSame(now, "day"),
+          isForCurrentMonth: day.isBetween(startDateDayBefore, endDateDayAfter)
         })
+      })
+      calendar.push(finalWeeks);
+    })
 
-        return calendar
-    }
+    return calendar
+  }
 
-    getCurrentMonth(date: any): string {
-        return this.months()[date.iMonth()]
-    }
+  getCurrentMonth(date: any): string {
+    return this.months()[date.iMonth()]
+  }
 
-    getCurrentYear(date: any): string {
-        return date.format('iYYYY')
-    }
+  getCurrentYear(date: any): string {
+    return date.format('iYYYY')
+  }
+
+  loadDaysInMonthWithYearAndMonth(iYear: number, iMonth: number): any[] {
+    const date = moment(momentHijri(`${iYear}/${iMonth + 1}/01`, 'iYYYY/iMM/iDD').format('YYYY/MM/DD'))
+    return this.daysInMonth(date.format('YYYY/MM/DD'))
+  }
 }
